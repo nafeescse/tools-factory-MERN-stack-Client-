c97221f6db6b60e29b3d7efe6be101e5a373ae0c
Nafees <abdur17@student.sust.edu>
2022-05-26 23:57:55 +0600

    final

--- src/pages/Shared/Footer.js
+++ src/pages/Shared/Footer.js
@@ -1,27 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
         <footer class="footer p-10 bg-neutral text-neutral-content">
             <div>
                 <span class="footer-title">Services</span>
-                <a class="link link-hover">Branding</a>
-                <a class="link link-hover">Design</a>
-                <a class="link link-hover">Marketing</a>
-                <a class="link link-hover">Advertisement</a>
+                <Link class="link link-hover">Branding</Link>
+                <Link class="link link-hover">Design</Link>
+                <Link class="link link-hover">Marketing</Link>
+                <Link class="link link-hover">Advertisement</Link>
             </div>
             <div>
                 <span class="footer-title">Company</span>
-                <a class="link link-hover">About us</a>
-                <a class="link link-hover">Contact</a>
-                <a class="link link-hover">Jobs</a>
-                <a class="link link-hover">Press kit</a>
+                <Link class="link link-hover">About us</Link>
+                <Link class="link link-hover">Contact</Link>
+                <Link class="link link-hover">Jobs</Link>
+                <Link class="link link-hover">Press kit</Link>
             </div>
             <div>
                 <span class="footer-title">Legal</span>
-                <a class="link link-hover">Terms of use</a>
-                <a class="link link-hover">Privacy policy</a>
-                <a class="link link-hover">Cookie policy</a>
+                <Link class="link link-hover">Terms of use</Link>
+                <Link class="link link-hover">Privacy policy</Link>
+                <Link class="link link-hover">Cookie policy</Link>
             </div>
         </footer>
     );
